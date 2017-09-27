85b8ab880b5d8b846e32145116f28864aa84c5c6
Alitsiya Yusupova <alitsiya.yusupova@lookout.com>
2017-09-26 22:06:25 -0700

    Add counter with characters remaining for tweet out of 140

--- app/src/main/java/com/codepath/apps/restclienttemplate/activities/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/activities/ComposeActivity.java
@@ -4,19 +4,23 @@ import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputFilter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.codepath.apps.restclienttemplate.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import butterknife.OnTextChanged;
 
 public class ComposeActivity extends AppCompatActivity {
 
     @BindView(R.id.etComposeTweet) EditText etComposeTweet;
     @BindView(R.id.btnTweet) Button btnTweet;
+    @BindView(R.id.tvTweetSize) TextView tvTweetSize;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +28,9 @@ public class ComposeActivity extends AppCompatActivity {
         setContentView(R.layout.compose_tweet);
 
         ButterKnife.bind(this);
+        InputFilter[] filters = new InputFilter[1];
+        filters[0] = new InputFilter.LengthFilter(140);
+        tvTweetSize.setFilters(filters);
     }
 
     @OnClick(R.id.btnTweet)
@@ -33,5 +40,11 @@ public class ComposeActivity extends AppCompatActivity {
         returnIntent.putExtra("result", tweet);
         setResult(Activity.RESULT_OK, returnIntent);
         finish();
+    }
+
+    @OnTextChanged(R.id.etComposeTweet)
+    void onComposeTweetTextChanged() {
+        int charsLeft = 140 - etComposeTweet.getText().length();
+        tvTweetSize.setText(String.valueOf(charsLeft));
     }
 }
